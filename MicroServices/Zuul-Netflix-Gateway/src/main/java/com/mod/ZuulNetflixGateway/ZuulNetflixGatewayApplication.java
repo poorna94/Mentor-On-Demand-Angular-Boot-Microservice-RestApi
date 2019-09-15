297b96b71c48fc3bebd921f240c4131f9dbcc4e3
makingout456 <kumardeep245@gmail.com>
2019-09-16 01:11:46 +0530

    @zuul gateway introduced

--- MicroServices/Zuul-Netflix-Gateway/src/main/java/com/mod/ZuulNetflixGateway/ZuulNetflixGatewayApplication.java
+++ MicroServices/Zuul-Netflix-Gateway/src/main/java/com/mod/ZuulNetflixGateway/ZuulNetflixGatewayApplication.java
@@ -1,16 +1,18 @@
-package com.mod.UserMicroservice;
+package com.mod.ZuulNetflixGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-public class UserMicroserviceApplication {
+@SpringBootApplication
+public class ZuulNetflixGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserMicroserviceApplication.class, args);
+		SpringApplication.run(ZuulNetflixGatewayApplication.class, args);
 	}
 
 }
